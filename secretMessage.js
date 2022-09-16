545f48fc7e2705ab329f6594a604862915c5cd24
U <urussell0802@gmail.com>
2022-09-16 04:55:52 -0400

    Update secretMessage.js

--- secretMessage.js
+++ secretMessage.js
@@ -1,16 +1,11 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
-console.log(secretMessage.length);
 secretMessage.pop();
-console.log(secretMessage.length);
 secretMessage.push('to' , 'Program')
 secretMessage[7] = 'right';
 secretMessage.shift();
 secretMessage.unshift('Programming');
 
-//console.log(secretMessage.indexOf('time,'))
-secretMessage.splice(6, 10 ,'know')
-//console.log(secretMessage);
+secretMessage.splice(6, 5 ,'know,')
 
 console.log(secretMessage.join(' '));
-
